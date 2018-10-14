7903dfe931894ca06dbbb0b85f1f1d426db41c8d
xwp <xwp_fullstack@163.com>
2018-10-14 20:55:07 +0800

    验证码

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,15 @@
+from info.utils.captcha.captcha import captcha
 from . import index_blue
 from info import redis_store
-from flask import render_template
+from flask import render_template,current_app
+
+# 获取返回一张图片
+@index_blue.route('/image_code')
+def image_code():
+    name,text,image_data =captcha.generate_captcha()
+    return image_data
+
+
 @index_blue.route('/')
 def HelloWorld():
     # 测试redis存储数据
@@ -17,3 +26,7 @@ def HelloWorld():
 
     return render_template("news/index.html")
 
+# 页面logo /favicon.ico
+@index_blue.route('/favicon.ico')
+def web_log():
+    return current_app.send_static_file("news/favicon.ico")
